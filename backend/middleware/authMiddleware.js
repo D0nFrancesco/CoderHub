3e0bcea75a304c22067a1f193f51ea40840f0ab9
xavier290 <xa25alex@gmail.com>
2022-02-23 14:47:27 -0600

    updating auth middleware and getting rid of the asyncHandler dependency

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,30 +1,24 @@
 const jwt = require('jsonwebtoken');
-const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 
-const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1];
-      // Verify token
+const protect = async(req, res) => {
+  try {
+    if (req.header.authorization && req.headers.authorization.startswith('Bearer')) {
+      let token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      // Get user from the token
+
       req.user = await User.findById(decoded.id).select('-password');
       next();
     }
-    catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error('Not authorized');
+    else {
+      res.status(401).json({message: 'unauthorized'});
+      return res.end();
     }
   }
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized, no token');
+  catch (err) {
+    res.status(500).write(err);
+    return res.end();
   }
-});
+}
 
 module.exports = { protect }
