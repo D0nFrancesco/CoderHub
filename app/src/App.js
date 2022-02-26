7adee4d8a929ec58450cdd177b6dfea3e7acf4f2
xavier290 <xa25alex@gmail.com>
2022-02-25 21:20:39 -0600

    fixing styling and html implementation

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-
-
 // styling
 import './styling/main.scss';
 
-// pages
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
+// pages
 import LandingPage from './pages/landing';
 import RegisterPage from './pages/register';
 import LoginPage from './pages/login';
@@ -15,51 +13,21 @@ import HomePage from './pages/home';
 import UserProfilePage from './pages/userProfilePage';
 
 class App extends Component {
-state = {
-    data: null
-  };
-
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
-
   render() {
     return (
       <BrowserRouter>
-            
-              <div className="container">
-
-                <Routes>
-
-                  <Route  path="/" element={<LandingPage />} />
-                  <Route  path="/home" element={<HomePage />} />
-                  
-                  <Route  path="/register" element={<RegisterPage />} />
-                  <Route  path="/login" element={<LoginPage />} />                  
-                  
-                  <Route  path="/chat" element={<Chat />} />
-
-                  <Route  path="/user-profile" element={<UserProfilePage />} />
-                  
-                </Routes>
-
-              </div>
-      </BrowserRouter>
-        
-        
-            
+        <div className="container">
+          {/* we are gonna change this routing later */}
+          <Routes>
+            <Route  path="/" element={<LandingPage />} />
+            <Route  path="home" element={<HomePage />} />
+            <Route  path="register/*" element={<RegisterPage />} />
+            <Route  path="login" element={<LoginPage />} />                  
+            <Route  path="chat" element={<Chat />} />
+            <Route  path="user-profile" element={<UserProfilePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>    
     );
   }
 }
